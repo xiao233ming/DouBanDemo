9ff8a19a499deb4517fa4171f83f85760091eb0d
xiao233ming <dianziwanwan@163.com>
2016-05-23 20:42:00 +0800

    Initial commit

---
+++ app/src/main/java/com/app/doubandemo/PagerFragment.java
@@ -0,0 +1,50 @@
+package com.app.doubandemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by mingming on 2016/5/23.
+ */
+public class PagerFragment extends Fragment {
+
+    private int pageNum;
+    private TextView tv;
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     * <p/>
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View pager = inflater.inflate(R.layout.paper_fragment,container,false);
+
+        tv = (TextView) pager.findViewById(R.id.tv);
+        Bundle bundle = getArguments();
+        pageNum = bundle.getInt("page_num");
+        tv.setText("" + pageNum);
+        return pager;
+    }
+}
